82d0af7be1fb3acb4523bce66d195518215a2f30
Hyslan Silva <hyslansilva@gmail.com>
2024-02-05 11:20:31 -0300

    construção ramal agua

--- src/unitarios/ligacao_agua/m_ligacao_agua.py
+++ src/unitarios/ligacao_agua/m_ligacao_agua.py
@@ -8,19 +8,26 @@ class LigacaoAgua(BaseUnitario):
     '''Ramo de Ligações (Ramal) de água'''
 
     def reposicoes(self):
+        '''Reposições dos serviços de Ligação de água'''
         pass
 
     def ligacao_agua(self):
+        '''Ramal novo de água, avulsa.'''
         pass
 
     def tra_nv(self):
+        '''Troca de Ramal de água não visível'''
         pass
 
     def png(self):
+        '''Passado novo ramal para nova rede - Obra'''
         pass
 
     def subst_agua(self):
+        '''Substituição de ramal de água, tem adicional de suprimir
+        o ferrule da rede'''
         pass
 
     def tra_prev(self):
+        '''Troca de ramal de água preventiva'''
         pass
