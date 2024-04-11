545f0a0b9a4e695e819988c3794ba61e7cbb9317
Hyslan Silva Cruz <hyslansilva@gmail.com>
2024-04-11 11:07:57 -0300

    /fix: packages & setuptools

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-'''
+"""
     Author: Hyslan Silva Cruz
     Built for work by Hyslan on SAP GUi v7.40
     Client: SABESP
-    
-'''
+
+"""
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 from setuptools import setup, find_packages
 
 setup(
     name='VAL',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
-    install_requires=[
-        "numpy", "pandas", "openpyxl", "pywin32",
-        "rich", "SQLAlchemy", "selenium", "tqdm",
-        "imageio", "pygame", "python-dotenvpip "
-    ],
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'val=src.main:main',
@@ -22,7 +22,7 @@ setup(
     },
     author='Hyslan Silva Cruz',
     author_email='hyslansilva@gmail.com',
-    description='Valoração automática de ordens',
+    description='Valoração automática de ordens dentro do SAPGUI v7.40',
     long_description=open('README.md', encoding='latin1').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/hyslan/Val',
