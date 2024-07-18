0b02427296db82b8b605c4d23b50c4bc1258d211
Hyslan Silva Cruz <hyslansilva@gmail.com>
2024-07-18 16:20:10 -0300

    #fix: ternario conditional

--- src/sql_view.py
+++ src/sql_view.py
@@ -186,6 +186,8 @@ class Tabela:
         console.print("\n [b]Família escolhida: ", family_str)
         engine = sa.create_engine(self.connection_url)
         cnn = engine.connect()
+        # TSEs leave out the plumbing services by Iara.
+        chief_iara_orders = "'534200', '534300', '537000', '537100', '538000'" if contrato == "4600042975" else "'',"
         sql_command = ("SELECT Ordem, COD_MUNICIPIO FROM [LESTE_AD\\hcruz_novasp].[v_Hyslan_Valoracao] "
                        f"WHERE FAMILIA IN ({family_str}) AND Contrato = '{contrato}' "
                        f"AND TSE_OPERACAO_ZSCP NOT IN ( "
@@ -195,9 +197,8 @@ class Tabela:
                        "'250000', '209000', '605000', '605000', '263000', '255000', '254000', '282000', '265000', "
                        "'260000', '265000', '263000', '262000', '284500', '286000', '282500', "  # -- RAMAL ÁGUA 
                        # UNITÁRIO
-                       # TSEs leave out the plumbing services by Iara.
                        # Obeying Iara's orders
-                       f"{'534200', '534300', '537000', '537100', '538000' if contrato == "4600042975" else ''}"
+                       f"{chief_iara_orders}"
                        "'136000', '159000', '155000') "  # -- CRIAR LÓGICA
                        )
 
