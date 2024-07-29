a030e41bda09bf3faf46ebefde3df315452ac4ca
Hyslan Silva Cruz <hyslansilva@gmail.com>
2024-07-29 09:13:37 -0300

    testing

--- src/test/simple_tests.py
+++ src/test/simple_tests.py
@@ -5,5 +5,12 @@ import platform
 # -Sub Main--------------------------------------------------------------
 
 '''Função para conexão SAP'''
-pythoncom.CoInitialize()
-sapguiauto: win32com.client.CDispatch = win32com.client.GetObject("SAPGUI")
+# pythoncom.CoInitialize()
+# sapguiauto: win32com.client.CDispatch = win32com.client.GetObject("SAPGUI")
+
+try:
+    sapguiauto = win32com.client.GetObject(
+        "SapROTWR.SapROTWrapper").GetROTEntry("SAPGUI")
+    print("Objeto SAPGUI obtido com sucesso")
+except pythoncom.com_error as e:
+    print(f"Erro ao obter o objeto SAPGUI: {e}")
