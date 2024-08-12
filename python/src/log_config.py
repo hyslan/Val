bb43893464b7999eeb04778782c26aa2f710ff94
Hyslan Silva Cruz <hyslansilva@gmail.com>
2024-08-12 09:49:56 -0300

    Adjusted logging levels

--- python/src/log_config.py
+++ python/src/log_config.py
@@ -8,20 +8,24 @@ def setup_logging(log_directory="logs", log_level=logging.DEBUG):
         os.makedirs(log_directory)
 
     logger = logging.getLogger()
-    logger.setLevel(log_level)
+    logger.setLevel(log_level)  # Global Level's log
 
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
+    # Handler for rotations files
     file_handler = RotatingFileHandler(
         os.path.join(log_directory, "val_detailed_log.log"),
         maxBytes=10**6,
         backupCount=10
     )
     file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
 
+    # Console Handle
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
 
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
--- python/src/log_decorator.py
+++ python/src/log_decorator.py
@@ -10,7 +10,12 @@ def log_execution(func):
     def wrapper(*args, **kwargs):
         logger.debug(
             f"Entrando em {func.__name__} com args={args} e kwargs={kwargs}")
-        result = func(*args, **kwargs)
-        logger.debug(f"Saindo de {func.__name__} com resultado={result}")
-        return result
+        try:
+            result = func(*args, **kwargs)
+            logger.info(f"Saindo de {func.__name__} com resultado={result}")
+            return result
+        except Exception as e:
+            logger.error(
+                f"Erro em {func.__name__} com args={args} e kwargs={kwargs}", exc_info=True)
+            print(f"Erro ao executar a função capturada em log_execution: {e}")
     return wrapper
