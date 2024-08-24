4e177dc7fc796b5dad7035ebb712c9269468ebc7
Hyslan Silva Cruz <hyslansilva@gmail.com>
2024-08-23 21:04:37 -0300

    try to fix

--- python/src/sapador.py
+++ python/src/sapador.py
@@ -1,9 +1,13 @@
 """Módulo de start do SAP."""
+
+import logging
 import os
 import re
+import shlex  # Add this line
 import subprocess
 import time
 from collections.abc import Callable
+from pathlib import Path
 from typing import TYPE_CHECKING, Literal
 
 from dotenv import load_dotenv
@@ -12,7 +16,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
 if TYPE_CHECKING:
@@ -23,29 +27,31 @@ def down_sap() -> str:
     """Baixa o .tx do SAP."""
     load_dotenv()
     url: str = os.environ["URL"]
-    s: Service = Service(
-        "chromedriver.exe")
+    s: Service = Service("chromedriver.exe")
     opt: Options = Options()
     opt.add_argument("--headless=new")
     opt.add_argument("--allow-running-insecure-content")
     opt.add_argument("--ignore-certificate-errors")
-    opt.add_argument(
-        f'--unsafely-treat-insecure-origin-as-secure={os.environ["URL"]}')
-    opt.add_experimental_option("prefs", {
-        "download.default_directory": os.getcwd() + "\\shortcut",
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-
-    })
+    opt.add_argument(f'--unsafely-treat-insecure-origin-as-secure={os.environ["URL"]}')
+    opt.add_experimental_option(
+        "prefs",
+        {
+            "download.default_directory": Path.cwd() / "shortcut",
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+        },
+    )
     driver: WebDriver = webdriver.Chrome(service=s, options=opt)
     # Navegar até a página de login
     driver.get(url)
     wait: WebDriverWait = WebDriverWait(driver, 180)
     wait.until(
-        EC.element_to_be_clickable((
-            By.XPATH,
-            '//div[@title="SiiS"]',
-        )),
+        ec.element_to_be_clickable(
+            (
+                By.XPATH,
+                '//div[@title="SiiS"]',
+            ),
+        ),
     )
     btn_siis: WebElement = driver.find_element(
         By.XPATH,
@@ -55,17 +61,16 @@ def down_sap() -> str:
     wait.until(file_downloaded("tx.sap"))
     driver.quit()
     # Caminho para o arquivo "tx.sap"
-    caminho_arquivo: str = os.getcwd() + "\\shortcut\\tx.sap"
+    caminho_arquivo: str = str(Path.cwd() / "shortcut" / "tx.sap")
     # Get the token SSO
-    with open(caminho_arquivo) as f:
+    with Path(caminho_arquivo).open() as f:
         txt = f.read()
 
     scan = re.search(r'at="MYSAPSSO2=(.*)"', txt)
-    token = scan.group(1)
+    token = scan.group(1) if scan is not None else ""
     # Tenta executar o comando
     try:
-        subprocess.run(["powershell", "start", '"' + caminho_arquivo + '"'],
-                       shell=True, check=False)
+        subprocess.run(["powershell", "start", shlex.quote(caminho_arquivo)], check=False)
         time.sleep(5)
         # Verifica se o processo está em execução
         if not is_process_running("powershell.exe"):
@@ -73,15 +78,16 @@ def down_sap() -> str:
     except subprocess.CalledProcessError:
         pass
     except Exception:
-        pass
+        logging.exception("")
 
     return token
 
 
 def file_downloaded(filename: str) -> Callable[[WebDriver], Literal[False] | bool]:
     """Verifica se o arquivo foi baixado completamente."""
+
     def predicate(driver: WebDriver) -> Literal[False] | bool:
-        files: list[str] = os.listdir(os.getcwd() + "\\shortcut\\")
+        files: list[str] = os.listdir(Path.cwd() / "shortcut")
         return any(file.endswith(filename) for file in files)
 
     return predicate
@@ -90,8 +96,8 @@ def file_downloaded(filename: str) -> Callable[[WebDriver], Literal[False] | boo
 def is_process_running(process_name: str) -> bool | None:
     """Verifica se o processo está em execução."""
     try:
-        subprocess.check_output(
-            f'tasklist /FI "IMAGENAME eq {process_name}"', shell=True)
+        subprocess.check_output(["tasklist", "/FI", f"IMAGENAME eq {shlex.quote(process_name)}"])
         return True
     except subprocess.CalledProcessError:
-        return False
+        pass
+    return False
